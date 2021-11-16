1f166e5f279d45dc42d66c2e7277c203f6a92328
David Polania <davidpolaniaac@users.noreply.github.com>
2021-11-16 08:37:42 -0500

    add website template

--- generators/website/index.js
+++ generators/website/index.js
@@ -1,23 +1,23 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(
-        `Welcome to the sublime ${chalk.red('generator-azure-devops-extension')} generator!`
-      )
+      yosay(`Welcome to ${chalk.green("create the website")} generator!`)
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: "input",
+        name: "websiteid",
+        message: "website ID",
+        default: "example-website-id",
+        validate: this.validateId.bind(this)
       }
     ];
 
@@ -28,13 +28,38 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    const websitePath = path.join("websites");
+    this.fs.copyTpl(
+      this.templatePath("**/*"),
+      this.destinationPath(websitePath)
     );
   }
 
   install() {
-    this.installDependencies();
+    const npmdir = path.join(process.cwd(), "websites");
+    process.chdir(npmdir);
+
+    this.installDependencies({
+      npm: true,
+      bower: false,
+      yarn: false
+    });
+  }
+
+  validateNotEmpty(input) {
+    return (input && Boolean(input.trim())) || "Cannot be left empty";
+  }
+
+  validateId(input) {
+    const notEmpty = this.validateNotEmpty(input);
+    const pattern = /^[a-z]+(?:-[a-z]+)*$/;
+    if (typeof notEmpty === "string") {
+      return notEmpty;
+    }
+
+    return (
+      (input && input.indexOf(" ") < 0 && pattern.test(input)) ||
+      "No spaces allowed, only [a-z]+(?:-[a-z]+)*$"
+    );
   }
 };
